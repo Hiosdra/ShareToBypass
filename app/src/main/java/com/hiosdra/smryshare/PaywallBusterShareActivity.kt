7949168a592516525d3ae6ce7f7542edfcbbbaaa
Oskar Drozda <themppsplx@gmail.com>
2025-10-27 22:36:30 +0100

    Refactor URL handling and improve code readability

--- app/src/main/java/com/hiosdra/smryshare/PaywallBusterShareActivity.kt
+++ app/src/main/java/com/hiosdra/smryshare/PaywallBusterShareActivity.kt
@@ -1,11 +1,7 @@
 package com.hiosdra.smryshare
 
-import java.net.URLEncoder
-
 class PaywallBusterShareActivity : BaseShareActivity() {
-    override fun buildTargetUrl(url: String): String {
-        val encodedUrl = URLEncoder.encode(url, "UTF-8")
-        return "https://paywallbuster.com/articles?article=$encodedUrl"
-    }
+    override fun buildTargetUrl(url: String): String =
+        "https://paywallbuster.com/articles?article=${encodeUrl(url)}"
 }
 
